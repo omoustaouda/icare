712a1e907e3d018e3aadb4966e28fcec3eb7024e
Othmane <othmane.moustaouda@gmail.com>
2016-02-15 03:58:28 +0100

    Fixed typo in devise.rb init conf

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -101,7 +101,7 @@ Devise.setup do |config|
   config.pepper = Rails.application.secrets.devise_pepper
 
   # Send a notification email when the user's password is changed
-+ # config.send_password_change_notification = false
+  # config.send_password_change_notification = false
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
